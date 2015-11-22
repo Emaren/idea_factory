5d57c247c8725f7f3202c226a82ed6f52922c380
Tony Blum <tonyblum@me.com>
2015-11-22 08:15:30 -0800

    CRUD implemented

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,5 +1,7 @@
 class IdeasController < ApplicationController
 
+  before_action :find_idea, only: [:show, :edit, :update, :destroy]
+
   def index
     @ideas = Idea.all.order("created_at DESC")
   end
@@ -10,6 +12,40 @@ class IdeasController < ApplicationController
 
   def create
     @idea = Idea.new idea_params
+    if @idea.save
+    redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @idea.update idea_params
+      redirect_to root_path
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @idea.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def find_idea
+    @idea = Idea.find params[:id]
+  end
+
+  def idea_params
+    params.require(:idea).permit(:title, :content)
   end
 
 end
