2f05607d95392d7a5ecd102ebd67245033ec07e8
Tony Blum <tonyblum@me.com>
2015-11-22 09:17:17 -0800

    Devise Idea implemented

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -2,16 +2,20 @@ class IdeasController < ApplicationController
 
   before_action :find_idea, only: [:show, :edit, :update, :destroy]
 
+  before_action :authenticate_user!
+
   def index
     @ideas = Idea.all.order("created_at DESC")
   end
 
   def new
-    @idea = Idea.new
+    @idea = current_user.ideas.build
+
   end
 
   def create
-    @idea = Idea.new idea_params
+    @idea = current_user.ideas.build idea_params
+
     if @idea.save
     redirect_to root_path
     else
